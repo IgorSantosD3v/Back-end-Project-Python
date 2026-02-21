36479e7186d2a0b677b362820a1cc524d8bf703c
Igor <okallizn@gmail.com>
2026-02-21 18:18:07 -0300

    Adicionando configurações adicionais

--- main.py
+++ main.py
@@ -71,13 +71,16 @@ def get_livros(page: int = 1, limit: int = 10, credentials: HTTPBasicCredentials
   if not meu_livrozinhos:
       return {"message": "Não existe nenhum livro!"}
   
+  livros_ordenados = sorted(meu_livrozinhos.items(), key=lambda x: x[0])
+  
+  
   start = (page - 1) * limit
   end = start + limit
       
   
   livros_paginados = [
       {"id": id_livro, "nome_livro": livro_data["nome_livro"], "autor_livro": livro_data["autor_livro"], "ano_livro": livro_data["ano_livro"]}
-      for id_livro, livro_data in list(meu_livrozinhos.items())[start:end]
+      for id_livro, livro_data in livros_ordenados [start:end]
   ]
   
   return{
